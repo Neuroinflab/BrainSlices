492b7f2e66f69b0882e11add51ed07761c302cab
Jakub Kowalski <j.kowalski@nencki.gov.pl>
2014-05-12 14:53:24 +0000

    code made more sparse; auxilary methods registerChange and _getPrivate provided
    git-svn-id: https://xp-dev.com/svn/crackOS/branches/interfaceRework@249 2def0f6a-7ed5-4680-9172-106844e829bd

--- server/static/js/scope.js
+++ server/static/js/scope.js
@@ -1,29 +1,74 @@
 /*
  * Implementacja scope - obiektu który trzyma globalne zmienne o stanie, i o ich zmianie powiadamia wszystkich słuchających.
  */
-(function(BS){
-    var listeners = new Array();
-    var variables = new Array();
-    var locks = new Array();
-    
-    BS.scope = {
-        register:function(listener){
-            listeners.push(listener);
-        },
-        set:function(variable,val){
-            if( locks[variable] != true){
-                locks[variable] = true;
-            variables[variable]=val;
-         
-            listeners.map( function(item){
-                item.change(variable, val);
-            });
-            locks[variable] = false;
-            }
-        },
-        get:function(variable){
-            return variables[variable];
-        }
-    };
+(function(BS)
+{
+  var listeners = new Array();
+  var variables = new Array();
+  var locks = new Array();
+  
+  BS.scope =
+  {
+    register:
+    function(listener)
+    {
+      listeners.push(listener);
+    },
+
+    registerChange:
+    /**
+     * Function: registerChange
+     *
+     * Create and register a brand new listener.
+     *
+     * Parameters:
+     *   f - a change method of the listener.
+     ********************************************/
+    function(f)
+    {
+      this.register({change: f});
+    },
+
+    set:
+    function(variable,val)
+    {
+      if( locks[variable] != true)
+      {
+        locks[variable] = true;
+        variables[variable]=val;
+     
+        listeners.map( function(item)
+        {
+          item.change(variable, val);
+        });
+
+        locks[variable] = false;
+      }
+    },
+
+    get:
+    function(variable)
+    {
+      return variables[variable];
+    },
+
+    /**
+     * Function: _getPrivate
+     *
+     * A debug method.
+     *
+     * Returns:
+     *   An object for monitoring private variables.
+     ***********************************************/
+    _getPrivate:
+    function()
+    {
+      return {
+        listeners: listeners,
+        variables: variables,
+        locks: locks
+      }
+    }
+  };
     
 } (BrainSlices));
