e592f239528fda8da49a21c949210132ccaa5ec0
Jakub Kowalski <j.kowalski@nencki.gov.pl>
2014-10-10 13:37:40 +0000

    packages listed in alphabetical order
    git-svn-id: https://xp-dev.com/svn/crackOS/branches/production@477 2def0f6a-7ed5-4680-9172-106844e829bd

--- INSTALL.sh
+++ INSTALL.sh
@@ -111,7 +111,7 @@ askBool ()
 if askBool "Do you want to fetch required packages?
 (Operation requires sudo privileges.)" N #FETCH_PACKAGES
   then
-    sudo apt-get install authbind libmagick++-dev libboost-thread-dev libboost-system-dev python-cherrypy3 postgresql libssl-dev python-psycopg2 at imagemagick
+    sudo apt-get install at authbind imagemagick libboost-system-dev libboost-thread-dev libmagick++-dev libssl-dev postgresql python-cherrypy3 python-psycopg2
     # python-cherrypy # Ubuntu 13.10
     # postgresql-contrib postgresql-client
   fi
